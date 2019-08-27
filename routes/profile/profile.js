aabcae2c965c7181f6085a1ca389217d7d263b75
Thies <thies2003@gmail.com>
2019-08-27 20:49:29 +0200

    Should all be with await now

--- routes/profile/profile.js
+++ routes/profile/profile.js
@@ -1,20 +1,15 @@
-const con = require("../../shared/database").con
+const {getUser} = require("../../shared/database")
 const config = require('../../shared/config')
 
-function profile(req, res) {
+async function profile(req, res) {
     console.log("Profile post")
     if (req.body.email === req.decoded.email) {
-        con.query("SELECT * FROM users WHERE email = ?", [req.decoded.email], function (err, result) {
-            if (err) {
-                res.status(404).end()
-                throw err
-            }
-            if (result[0]) {
+        var result = await getUser(req.decoded.email)
+            if (result) {
                 res.send({ ok: true, data: result.data })
             } else {
                 res.send({ ok: false, msg: config.errors.notFound })
             }
-        })
     } else {
         res.send({ ok: false, error: config.errors.general })
     }
--- routes/profile/refreshAccess.js
+++ routes/profile/refreshAccess.js
@@ -1,49 +1,45 @@
 const security = require('../../shared/security')
-const con = require("../../shared/database").con
+const {getUser} = require("../../shared/database")
 const config = require('../../shared/config')
 
-function refreshAccess(req, res) {
+async function refreshAccess(req, res) {
     var body = req.body
     console.log("Valid cookie in function refresh token: ", req.cookies.refreshtoken ? "yes" : "no")
     if (body.email && req.cookies.refreshtoken) {
         //First check if user exists
         if (security.emailRegex.test(body.email)) {
-            con.query("SELECT * FROM users WHERE email = ?", [body.email], function (err, result) {
-                if (err) {
-                    res.status(404).end()
-                    throw err
-                }
-                if (result[0]) {
-                    console.log("Refreshing access for: ", result[0].id)
-                    security.jwt.verify(req.cookies.refreshtoken, config.secret, (err, decoded) => {
-                        if (err) {
-                            return res.json({
-                                ok: false,
-                                message: config.errors.invalidToken
-                            });
+            var result = await getUser(body.email)
+            if (result) {
+                console.log("Refreshing access for: ", result.id)
+                security.jwt.verify(req.cookies.refreshtoken, config.secret, (err, decoded) => {
+                    if (err) {
+                        return res.json({
+                            ok: false,
+                            message: config.errors.invalidToken
+                        });
+                    } else {
+                        var refreshArray = JSON.parse(result.token)
+                        var newresult = refreshArray.find(x => x.token === decoded.refreshtoken);
+                        // console.log(refreshArray, decoded.refreshtoken, newresult)
+                        if (newresult) {
+                            console.log("Cookie verified")
+                            let accesstoken = security.jwt.sign({ email: body.email, id: result[0].id },
+                                config.secret,
+                                {
+                                    expiresIn: config.accessExpiry
+                                }
+                            );
+                            res.cookie("accesstoken", accesstoken, { expires: new Date(Date.now() + config.accessExpiry), httpOnly: true, path: "/user/" })
+                            res.send({ ok: true })
                         } else {
-                            var refreshArray = JSON.parse(result[0].token)
-                            var newresult = refreshArray.find( x => x.token === decoded.refreshtoken );
-                            // console.log(refreshArray, decoded.refreshtoken, newresult)
-                            if (newresult) {
-                                console.log("Cookie verified")
-                                let accesstoken = security.jwt.sign({ email: body.email, id: result[0].id },
-                                    config.secret,
-                                    {
-                                        expiresIn: config.accessExpiry
-                                    }
-                                );
-                                res.cookie("accesstoken", accesstoken, { expires: new Date(Date.now() + config.accessExpiry), httpOnly: true, path: "/user/" })
-                                res.send({ ok: true })
-                            } else {
-                                res.send({ ok: false, msg: config.errors.noRefresh })
-                            }
+                            res.send({ ok: false, msg: config.errors.noRefresh })
                         }
-                    });
-                } else {
-                    res.send({ ok: false, msg: config.errors.notFound })
-                }
-            })
+                    }
+                });
+            } else {
+                res.send({ ok: false, msg: config.errors.notFound })
+            }
+
         } else {
             res.send({ ok: false, msg: config.errors.regexNotMatch })
         }
