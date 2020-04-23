c1939adeffad5d703b92c775639a6f1be4e13901
Thies Nieborg <thies2003@gmail.com>
2020-04-23 12:24:48 +0200

    Fixed dev_start script, regenerated package lock

--- dev_start.sh
+++ dev_start.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
+
+exit_commands() {
+    printf "\rSIGINT caught. Stopping docker container \n"
+    docker stop test-mongodb
+    exit
+}
+
+trap 'exit_commands' SIGINT
+
 export JWT_SECRET=LongJwtSecret;
 
 docker start test-mongodb || docker run --name test-mongodb -d -p 27017:27017 -v `pwd`/mongoData:/data/db mongo
 cd src
 nodemon index.js
-docker stop test-mongodb
